b94268cb8e0247ea5a3a211d83d092c8ea70127b
Hossein <jadidi.hosein@gmail.com>
2026-01-01 21:54:04 +0800

    update issues

--- src/app/components/Support.tsx
+++ src/app/components/Support.tsx
@@ -210,7 +210,7 @@ export function Support() {
             </div>
           </div>
           <div className="pt-8 border-t border-slate-800 text-center text-slate-400">
-            <p>&copy; 2025 CareTrack. All rights reserved. Made by Gecko lovers for Gecko lovers</p>
+            <p>&copy; 2025 CareTrack. All rights reserved. Made by gecko lovers for gecko lovers</p>
           </div>
         </div>
       </footer>
